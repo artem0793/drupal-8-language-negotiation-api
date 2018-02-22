f9f2daffa8089e38842f12e68c2079407b762932
Vilkov Artem <artem.v@team.anyforsoft.com>
2018-02-22 17:20:13 +0200

    Added default country locations

--- web/sites/default/default.settings.php
+++ web/sites/default/default.settings.php
@@ -783,3 +783,8 @@ $settings['entity_update_batch_size'] = 50;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+/**
+ * Default content folder.
+ */
+$config['content_directory'] = '../content';
